b77a7c0854e886941b66c1a5ebc2739fb258373b
Yuryi <yuryi@cosmtsk.ru>
2023-09-29 07:11:43 +0000

    возвращаем 12 кГц

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -391,9 +391,9 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 10;
+  htim1.Init.Prescaler = 0;//10;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 6400; // 1200 Hz todo
+  htim1.Init.Period = 7000;//6400; // 1200 Hz todo
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV4;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
