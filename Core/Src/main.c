a8d18f9d3bc362532a3f58aed5405270e67d3281
Yuryi <yuryi@cosmtsk.ru>
2023-11-22 11:08:20 +0000

    проблема с синхронизацией пилы и ШИМ на двух ячейках

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -327,7 +327,7 @@ static void MX_TIM1_Init(void)
   htim1.Instance = TIM1;
   htim1.Init.Prescaler = 0;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 7900; // 10.66 êÃö
+  htim1.Init.Period = 7800; // 10.66 êÃö
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
