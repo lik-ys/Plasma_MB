1497772f6c513d8c73f9b68f2e8b3be04480b7cf
luch <lucheprom@yandex.ru>
2024-02-01 10:42:47 +0000

    BUG: при опросе всех ячеек он останавливается

--- Core/_Protocols/MB/mb_com.cpp
+++ Core/_Protocols/MB/mb_com.cpp
@@ -167,7 +167,7 @@ bool ModBusCom::Read( void )
     mb_act.response = 0;
     
     if ( saddr++ >= 6 )  saddr = 1;
-    this->addr  = static_cast<mb_addr_t>(4);
+    this->addr  = static_cast<mb_addr_t>(saddr);
     res = Hr_query( this->addr, static_cast<eMBReg_t>(REG_R_CURR_1s) );
   }else;// ret = false;  
   return res;
