f10a1bdbae84668b5eb5a7014bc2edbf67eb3043
luch <lucheprom@yandex.ru>
2024-02-07 10:41:20 +0000

    время простоя сократил

--- Core/_Protocols/MB/include/mb_m.h
+++ Core/_Protocols/MB/include/mb_m.h
@@ -417,7 +417,7 @@ BOOL eMBMasterIsEnabled( void );
 uint16_t GetDataMBRg( USHORT slave_addr, uint16_t reg );
 BOOL IsMBRcvSate (void );
 
-#define MB_CNT_ERROR 	15000
+#define MB_CNT_ERROR 	7000
 
 void SetRcvIdleState (void);
 
